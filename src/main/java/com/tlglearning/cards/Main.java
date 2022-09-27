edbf3abc025dc0f78924dbacb8e8c8a9cc33db45
nick-bennett <nick@nickbenn.com>
2022-09-27 14:37:21 -0600

    ColorFirst as inner class in Main (in-progress).

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -20,20 +20,21 @@ public class Main {
     System.out.println(deck);
   }
 
-}
+  private class ColorFirst implements Comparator<Card> {
 
-class ColorFirst implements Comparator<Card> {
-
-  @Override
-  public int compare(Card card1, Card card2) {
-    int comparison = card1.suit().color().compareTo(card2.suit().color());
-    if (comparison == 0) {
-      comparison = card1.suit().compareTo(card2.suit());
+    @Override
+    public int compare(Card card1, Card card2) {
+      int comparison = card1.suit().color().compareTo(card2.suit().color());
       if (comparison == 0) {
-        comparison = - card1.rank().compareTo(card2.rank());
+        comparison = card1.suit().compareTo(card2.suit());
+        if (comparison == 0) {
+          comparison = - card1.rank().compareTo(card2.rank());
+        }
       }
+      return comparison;
     }
-    return comparison;
+
   }
 
 }
+
